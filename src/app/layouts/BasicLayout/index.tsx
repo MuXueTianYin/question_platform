cb445e3a9590e7e5dbad91728506f7119fe607c0
muxuetianyin <794971506@qq.com>
2024-11-18 18:02:47 +0800

    feat

--- src/app/layouts/BasicLayout/index.tsx
+++ src/app/layouts/BasicLayout/index.tsx
@@ -1,16 +1,8 @@
 "use client"
-import {
-    GithubFilled,
-    InfoCircleFilled,
-    LogoutOutlined,
-    PlusCircleFilled,
-    QuestionCircleFilled,
-    SearchOutlined,
-} from '@ant-design/icons';
-import type {ProSettings} from '@ant-design/pro-components';
+import {GithubFilled, InfoCircleFilled, LogoutOutlined, QuestionCircleFilled, SearchOutlined,} from '@ant-design/icons';
 import {ProConfigProvider, ProLayout,} from '@ant-design/pro-components';
 import {Dropdown, Input, theme,} from 'antd';
-import React, {useState} from 'react';
+import React from 'react';
 import {Props} from "next/script";
 import Image from "next/image";
 import {usePathname} from "next/navigation";
@@ -65,105 +57,118 @@ export default function  BasicLayout ({children}: Props): React.ReactElement  {
             }}
         >
             <ProConfigProvider hashed={false}>
-                    <ProLayout
-                        prefixCls="my-prefix"
-                        location={{
-                            pathname,
-                        }}
-                        token={{
-                            header: {
-                                colorBgMenuItemSelected: 'rgba(0,0,0,0.04)',
-                            },
-                        }}
-                        layout={'top'}
-                        title={"面试刷题平台"}
-                        logo={
+                <ProLayout
+                    prefixCls="my-prefix"
+                    location={{
+                        pathname,
+                    }}
+                    token={{
+                        header: {
+                            colorBgMenuItemSelected: 'rgba(0,0,0,0.04)',
+                        },
+                    }}
+                    layout={'top'}
+                    title={"面试刷题平台"}
+                    logo={
                         <Image src="/assets/logo.png" width={32} height={32} alt={"muxuetianyin-刷题网站"} />
-                        }
-                        siderMenuType="group"
-                        menu={{
-                            collapsedShowGroupTitle: true,
-                        }}
-                        avatarProps={{
-                            src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
-                            size: 'small',
-                            title: '七妮妮',
-                            render: (props, dom) => {
-                                return (
-                                    <Dropdown
-                                        menu={{
-                                            items: [
-                                                {
-                                                    key: 'logout',
-                                                    icon: <LogoutOutlined />,
-                                                    label: '退出登录',
-                                                },
-                                            ],
-                                        }}
-                                    >
-                                        {dom}
-                                    </Dropdown>
-                                );
+                    }
+                    siderMenuType="group"
+                    menu={{
+                        collapsedShowGroupTitle: true,
+                    }}
+                    menuDataRender={()=>{
+                        return [
+                            {
+                                path:'questions',
+                                name:'题目'
                             },
-                        }}
-                        actionsRender={(props) => {
-                            if (props.isMobile) return [];
-                            if (typeof window === 'undefined') return [];
-                            return [
-                                props.layout !== 'side' && document.body.clientWidth > 1400 ? (
-                                    <SearchInput />
-                                ) : undefined,
-                                <InfoCircleFilled key="InfoCircleFilled" />,
-                                <QuestionCircleFilled key="QuestionCircleFilled" />,
-                                <GithubFilled key="GithubFilled" />,
-                            ];
-                        }}
-                        headerTitleRender={(logo, title, _) => {
-                            const defaultDom = (
-                                <a>
-                                    {logo}
-                                    {title}
-                                </a>
-                            );
-                            if (typeof window === 'undefined') return defaultDom;
-                            if (document.body.clientWidth < 1400) {
-                                return defaultDom;
+                            {
+                                path:'Banks',
+                                name:'题库'
                             }
-                            if (_.isMobile) return defaultDom;
+                        ]
+                    }}
+                    avatarProps={{
+                        src: 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
+                        size: 'small',
+                        title: '七妮妮',
+                        render: (props, dom) => {
                             return (
-                                <>
-                                    {defaultDom}
-                                </>
-                            );
-                        }}
-                        menuFooterRender={(props) => {
-                            if (props?.collapsed) return undefined;
-                            return (
-                                <div
-                                    style={{
-                                        textAlign: 'center',
-                                        paddingBlockStart: 12,
+                                <Dropdown
+                                    menu={{
+                                        items: [
+                                            {
+                                                key: 'logout',
+                                                icon: <LogoutOutlined />,
+                                                label: '退出登录',
+                                            },
+                                        ],
                                     }}
                                 >
-                                    <div>© 2021 Made with love</div>
-                                    <div>by Ant Design</div>
-                                </div>
+                                    {dom}
+                                </Dropdown>
                             );
-                        }}
-                        onMenuHeaderClick={(e) => console.log(e)}
-                        menuItemRender={(item, dom) => (
+                        },
+                    }}
+                    actionsRender={(props) => {
+                        if (props.isMobile) return [];
+                        if (typeof window === 'undefined') return [];
+                        return [
+                            props.layout !== 'side' && document.body.clientWidth > 1400 ? (
+                                <SearchInput />
+                            ) : undefined,
+                            <InfoCircleFilled key="InfoCircleFilled" />,
+                            <QuestionCircleFilled key="QuestionCircleFilled" />,
+                            <GithubFilled key="GithubFilled" />,
+                        ];
+                    }}
+                    headerTitleRender={(logo, title, _) => {
+                        const defaultDom = (
+                            <a>
+                                {logo}
+                                {title}
+                            </a>
+                        );
+                        // if (typeof window === 'undefined') return defaultDom;
+                        if (document.body.clientWidth < 1400) {
+                            return defaultDom;
+                        }
+                        if (_.isMobile) return defaultDom;
+                        return (
+                            <>
+                                {defaultDom}
+                            </>
+                        );
+                    }}
+                    menuFooterRender={(props) => {
+                        if (props?.collapsed) return undefined;
+                        return (
                             <div
-                                onClick={() => {
-                                    // setPathname(item.path || '/welcome');
+                                style={{
+                                    textAlign: 'center',
+                                    paddingBlockStart: 12,
                                 }}
                             >
-                                {dom}
+                                <div>© 2021 Made with love</div>
+                                <div>by Ant Design</div>
                             </div>
-                        )}
-                    >
-                        {children}
-                    </ProLayout>
+                        );
+                    }}
+                    onMenuHeaderClick={(e) => console.log(e)}
+                    menuItemRender={(item, dom) => (
+                        <div
+                            onClick={() => {
+                                // setPathname(item.path || '/welcome');
+                            }}
+                        >
+                            {dom}
+                        </div>
+                    )}
+                >
+                    {children}
+                </ProLayout>
             </ProConfigProvider>
         </div>
+
     );
 };
